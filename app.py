44ffc22333d4ffaf7bddbd1145d32be3d79684ad
khriemer2596 <khriemer@gmail.com>
2023-05-16 11:15:51 -0500

    Renamed home page to index

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app.debug = True
 
 @app.route("/", methods=["GET"])  # route for home page
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 
 @app.route("/microservice", methods=["POST"])
